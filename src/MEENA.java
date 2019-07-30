ad1e6d222e947d55810fac052f7d11a7253723f8
spandy021987 <52914892+spandy021987@users.noreply.github.com>
2019-07-30 15:41:41 +0530

    Update MEENA.java

--- src/MEENA.java
+++ src/MEENA.java
@@ -4,6 +4,11 @@ public class MEENA {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		System.out.println("Hi World");
+		System.out.println("bye World");
+		System.out.println("Hello tomorrow");
+		
+		
 
 	}
 
